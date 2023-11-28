d4f466a924ae29504977d628ec6f80e577294457
Sangjoon Han <jphan32@me.com>
2023-11-28 22:17:59 +0900

    Add: Template

---
+++ config_TEMPLATE.py
@@ -0,0 +1,7 @@
+pclist = [
+    {"name": "Intel NUC", "ip": "192.168.50.111", "mac_addr":"48:__:__:__:__:__", "can_wol":True, "status":False},
+    {"name": "GPU Server", "ip": "192.168.50.112", "mac_addr":"58:__:__:__:__:__", "can_wol":True, "status":False},
+    {"name": "Xavier", "ip": "192.168.50.113", "mac_addr":"00:__:__:__:__:__", "can_wol":True, "status":False},
+    {"name": "Synology NAS", "ip": "192.168.50.114", "mac_addr":"00:__:__:__:__:__", "can_wol":True, "status":False},
+    {"name": "Mac Pro", "ip": "192.168.50.115", "mac_addr":"9C:__:__:__:__:__", "can_wol":False, "status":False},
+]
